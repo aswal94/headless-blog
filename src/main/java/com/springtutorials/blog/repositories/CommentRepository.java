2e839dcd2b008e8cac6a83661a33f46ac347c69f
Amit Aswal <aswal.amit94@gmail.com>
2022-07-14 23:34:41 +0530

    Added annotation

--- src/main/java/com/springtutorials/blog/repositories/CommentRepository.java
+++ src/main/java/com/springtutorials/blog/repositories/CommentRepository.java
@@ -2,6 +2,8 @@ package com.springtutorials.blog.repositories;
 
 import com.springtutorials.blog.model.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 }

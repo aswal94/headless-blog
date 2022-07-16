c9ac31bc11977463cfb6d567117d16e787dc8f12
Amit Aswal <aswal.amit94@gmail.com>
2022-07-17 01:13:09 +0530

    Updated auth config

--- src/main/java/com/springtutorials/blog/config/WebSecurityConfig.java
+++ src/main/java/com/springtutorials/blog/config/WebSecurityConfig.java
@@ -40,10 +40,12 @@ class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf().disable()
-                .authorizeRequests().antMatchers("/authenticate").permitAll().
-                anyRequest().authenticated().and().
-                exceptionHandling().and().sessionManagement()
+                .authorizeRequests().antMatchers("/").permitAll()
+                //.anyRequest().authenticated()
+                .and()
+                .exceptionHandling().and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
         httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 
 

ccfb38b6cb610eb94dacf67351e305f38db9f70e
Amit Aswal <aswal.amit94@gmail.com>
2022-07-17 01:10:05 +0530

    Added jwt auth with spring security

---
+++ src/main/java/com/springtutorials/blog/model/User.java
@@ -0,0 +1,21 @@
+package com.springtutorials.blog.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "User")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String userName;
+    private String password;
+    private boolean active;
+    private String roles;
+}
---
+++ src/main/java/com/springtutorials/blog/services/impl/MyUserDetailsService.java
@@ -0,0 +1,34 @@
+package com.springtutorials.blog.services.impl;
+
+import com.springtutorials.blog.MyUserDetails;
+import com.springtutorials.blog.model.User;
+import com.springtutorials.blog.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByUserName(userName);
+
+        user.orElseThrow(() -> new UsernameNotFoundException("Not found: " + userName));
+
+        return user.map(MyUserDetails::new).get();
+    }
+}
